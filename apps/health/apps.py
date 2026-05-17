4bb07af11aede9a58ea05437e4389e17bc958a24
SebastianPenarandaNieto <sebastianpeni@ufps.edu.co>
2026-05-17 17:48:23 -0500

    ADD: Avances en los modelos de la app health

--- apps/health/apps.py
+++ apps/health/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class HealthConfig(AppConfig):
-    name = 'health'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.health"
+    verbose_name = "Health"
